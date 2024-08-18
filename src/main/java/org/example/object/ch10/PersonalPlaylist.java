aee0e6d922dd391e62f81bf95a226e4f66c6bddb
JunHwan Kim <jjunhwan-kim@naver.com>
2024-08-18 14:52:17 +0900

    Feat: [Ch10] Add ch10 base code
    - 상속과 코드 재사용 기본 코드

---
+++ src/main/java/org/example/object/ch10/PersonalPlaylist.java
@@ -0,0 +1,9 @@
+package org.example.object.ch10;
+
+public class PersonalPlaylist extends Playlist {
+
+    public void remove(Song song) {
+        getTracks().remove(song);
+        getSingers().remove(song.getSinger());
+    }
+}
